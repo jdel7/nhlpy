b12295237b6344e145111040b7bab5a6d9a9016b
Alexander Delgado <santosde@unlv.nevada.edu>
2018-05-23 16:43:10 -0700

    Set up for PyPI package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+        name = 'nhlpy',
+        packages = ['nhlpy'],
+        version = '0.1',
+        description = 'An easy to use NHL API wrapper',
+        author = 'Alexander Delgado',
+        author_email = 'jobehico@gmail.com',
+        url = 'https://github.com/0xalexdelgado/nhlpy',
+        download_url = 'https://github.com/0xalexdelgado/nhlpy/archive/0.1.tar.gz',
+        keywords = ['hockey', 'API', 'NHL', 'wrapper', 'api', 'nhlpy', 'statistics'],
+        classifiers = [],
+        )
+

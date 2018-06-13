fa935dcbf397644cef4ef3181565af3f9ff76e94
Jonathan Craig <blurr@iamtheblurr.com>
2018-06-12 21:56:06 -0700

    Minor PEP8 convention changes for setup.py.  Removed spaces surrounding equal signs.  Changed indenting from 8 spaces to 4.

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 from distutils.core import setup
 setup(
-        name = 'nhlpy',
-        packages = ['nhlpy'],
-        version = '0.3.0',
-        description = 'An easy to use NHL API wrapper',
-        author = 'Alexander Delgado',
-        author_email = 'jobehico@gmail.com',
-        url = 'https://github.com/0xalexdelgado/nhlpy',
-        download_url = 'https://github.com/0xalexdelgado/nhlpy/archive/0.2.tar.gz',
-        keywords = ['hockey', 'API', 'NHL', 'wrapper', 'api', 'nhlpy', 'statistics'],
-        classifiers = [],
-        )
-
+    name='nhlpy',
+    packages=['nhlpy'],
+    version='0.3.0',
+    description='An easy to use NHL API wrapper',
+    author='Alexander Delgado',
+    author_email='jobehico@gmail.com',
+    url='https://github.com/0xalexdelgado/nhlpy',
+    download_url='https://github.com/0xalexdelgado/nhlpy/archive/0.2.tar.gz',
+    keywords=['hockey', 'API', 'NHL', 'wrapper', 'api', 'nhlpy', 'statistics'],
+    classifiers=[], requires=['requests']
+)

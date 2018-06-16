b1f46ff876fdeef219a7d8d1dacf60899e949b40
Alexander Delgado <santosde@unlv.nevada.edu>
2018-06-15 21:28:34 -0700

    Add test for the info method in the Team module

--- tests/test_nhlpy.py
+++ tests/test_nhlpy.py
@@ -1,22 +1,50 @@
 # We expect that our nhlpy package will have a Team class
-import vcr
-import requests
-from pytest import fixture
-from nhlpy.team import Team
+from nhlpy import team
 
-@fixture
-def team_keys():
-    # Responsible only for returning the test data
-    return ['teams']
+def test_team_info():
 
-@vcr.use_cassette()
-def test_team_info(team_keys):
-    """ Tests an API call to get a Teams information """
-
-    team_instance = Team(1)
-    response = team_instance.info()
+    test_team = team.Team(54)
+    response = test_team.info()
 
     assert isinstance(response, dict)
-    assert response['teams']
-    assert set(team_keys).issubset(response.keys())
+    assert response['teams'] == [
+  {
+    'id': 54,
+    'name': 'Vegas Golden Knights',
+    'link': '/api/v1/teams/54',
+    'venue': {
+      'name': 'T-Mobile Arena',
+      'link': '/api/v1/venues/null',
+      'city': 'Las Vegas',
+      'timeZone': {
+        'id': 'America/Los_Angeles',
+        'offset': -7,
+        'tz': 'PDT'
+      }
+    },
+    'abbreviation': 'VGK',
+    'teamName': 'Golden Knights',
+    'locationName': 'Vegas',
+    'firstYearOfPlay': '2016',
+    'division': {
+      'id': 15,
+      'name': 'Pacific',
+      'link': '/api/v1/divisions/15'
+    },
+    'conference': {
+      'id': 5,
+      'name': 'Western',
+      'link': '/api/v1/conferences/5'
+    },
+    'franchise': {
+      'franchiseId': 38,
+      'teamName': 'Golden Knights',
+      'link': '/api/v1/franchises/38'
+    },
+    'shortName': 'Vegas',
+    'officialSiteUrl': 'http://www.vegasgoldenknights.com',
+    'franchiseId': 38,
+    'active': True
+  }
+]
 
